b3b50eb584d4b75e4c16b3838eda3241700161e5
YeuTongCheng <103835914+YeuTongCheng@users.noreply.github.com>
2022-08-03 16:00:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -75,3 +75,85 @@ def onbase_percentage(info, batting_stats):
         return (hits + walks) / (at_bats + walks)
     else:
         return 0
+def slugging_percentage(info, batting_stats):
+    """
+    Inputs:
+      batting_stats - dictionary of batting statistics (values are strings)
+    Output:
+      Returns the slugging percentage as a float
+    """
+    hits = float(batting_stats[info["hits"]])
+    doubles = float(batting_stats[info["doubles"]])
+    triples = float(batting_stats[info["triples"]])
+    home_runs = float(batting_stats[info["homeruns"]])
+    singles = hits - doubles - triples - home_runs
+    at_bats = float(batting_stats[info["atbats"]])
+    if at_bats >= MINIMUM_AB:
+        return (singles + 2 * doubles + 3 * triples + 4 * home_runs) / at_bats
+    else:
+        return 0
+
+
+##
+## Part 1: Functions to compute top batting statistics by year
+##
+
+def filter_by_year(statistics, year, yearid):
+    """
+    Inputs:
+      statistics - List of batting statistics dictionaries
+      year       - Year to filter by
+      yearid     - Year ID field in statistics
+    Outputs:
+      Returns a list of batting statistics dictionaries that
+      are from the input year.
+    """
+    by_year=[]
+    for element in statistics:
+        if element[yearid]==str(year):
+            by_year.append(element)
+    return by_year
+
+
+def top_player_ids(info, statistics, formula, numplayers):
+    """
+    Inputs:
+      info       - Baseball data information dictionary
+      statistics - List of batting statistics dictionaries
+      formula    - function that takes an info dictionary and a
+                   batting statistics dictionary as input and
+                   computes a compound statistic
+      numplayers - Number of top players to return
+    Outputs:
+      Returns a list of tuples, player ID and compound statistic
+      computed by formula, of the top numplayers players sorted in
+      decreasing order of the computed statistic.
+    """
+    result=[]
+    for element in statistics:
+        computed_statistic=formula(info, element)
+        result.append(tuple([element[info["playerid"]],computed_statistic]))    
+    result.sort(reverse=True,key=lambda pair:pair[1])
+    return result[:numplayers]
+
+def lookup_player_names(info, top_ids_and_stats):
+    """
+    Inputs:
+      info              - Baseball data information dictionary
+      top_ids_and_stats - list of tuples containing player IDs and
+                          computed statistics
+    Outputs:
+      List of strings of the form "x.xxx --- FirstName LastName",
+      where "x.xxx" is a string conversion of the float stat in
+      the input and "FirstName LastName" is the name of the player
+      corresponding to the player ID in the input.
+    """
+    play_names=[]
+    table=read_csv_as_list_dict(info['masterfile'], info['separator'], info['quote'])
+    for element in top_ids_and_stats:
+        index=next((i for i, item in enumerate(table) if item[info['playerid']]\
+                    == element[0]), None)
+        play_names.append("{:.3f}".format(element[1])+" --- "\
+                          +table[index][info['firstname']]+" "+table[index][info['lastname']])
+    return play_names
+
